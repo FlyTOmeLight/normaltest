de4cfb3d256ab40690c754c7d0d04ee0cd493bf8
FlyTOmeLight <zhoubohan.pro@gmail.com>
2023-02-09 06:37:22 +0800

    add copydir

--- copydir/go.mod
+++ copydir/go.mod
@@ -1,3 +1,37 @@
 module github.com/FlyTOmeLight/normaltest/copydir
 
 go 1.19
+
+require (
+	github.com/FlyTOmeLight/normaltest/filesystem v0.0.0-20230208213945-71dbe8bccb89
+	github.com/davecgh/go-spew v1.1.1
+	github.com/rclone/rclone v1.61.1
+)
+
+require (
+	github.com/abbot/go-http-auth v0.4.0 // indirect
+	github.com/aws/aws-sdk-go v1.44.197 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.7 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/jzelinskie/whirlpool v0.0.0-20201016144138-0675e54bb004 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil/v3 v3.22.10 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.4.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/term v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+	golang.org/x/time v0.2.0 // indirect
+)
--- copydir/main.go
+++ copydir/main.go
@@ -1,5 +1,89 @@
 package main
 
-func main() {
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/FlyTOmeLight/normaltest/filesystem"
+	"github.com/rclone/rclone/fs/accounting"
+	"github.com/rclone/rclone/fs/config"
+	"github.com/rclone/rclone/fs/config/configfile"
+	"github.com/rclone/rclone/fs/rc"
+)
+
+var (
+	flagLocalDir  = flag.String("l", "", "local")
+	flagRemoteDir = flag.String("r", "", "remote")
+	alias         = "devmodel02-8077"
+)
+
+func main() {
+	flag.Parse()
+	aliasCfg := loadRcloneConfig()[alias]
+	copyDirToRemote(aliasCfg, flagLocalDir, flagRemoteDir)
+}
+
+// @brief：耗时统计函数
+func timeCost() func() {
+	start := time.Now()
+	return func() {
+		tc := time.Since(start)
+		fmt.Printf("time cost = %v\n", tc)
+	}
+}
+
+func copyDirToRemote(aliasCfg interface{}, ld, rd *string) {
+	if ld == nil || rd == nil {
+		panic("local or remote dir is nil")
+	}
+	defer timeCost()
+	lbs, err := filesystem.NewBlobStore(filesystem.KindLocal, "/", nil)
+	if err != nil {
+		panic(err)
+	}
+	rbs, err := filesystem.NewBlobStore(filesystem.KindS3, "benchmark", map[string]string{
+		"ak":         aliasCfg.(rc.Params)["access_key_id"].(string),
+		"sk":         aliasCfg.(rc.Params)["secret_access_key"].(string),
+		"host":       aliasCfg.(rc.Params)["endpoint"].(string),
+		"region":     aliasCfg.(rc.Params)["region"].(string),
+		"disableSSL": "true",
+	})
+	if err != nil {
+		panic(err)
+	}
+	err = filepath.Walk(*ld, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		locRelativePath := strings.TrimPrefix(path, *ld)
+		realS3Path := fmt.Sprintf("%s%s", *rd, locRelativePath)
+		relativePath := strings.TrimPrefix(path, "/")
+		err = filesystem.CopyRaw(lbs, rbs, relativePath, realS3Path)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadRcloneConfig() rc.Params {
+	err := config.SetConfigPath("/Users/jojo/.config/rclone/rclone.conf")
+	if err != nil {
+		panic(err)
+	}
+	err = config.Data().Load()
+	if err != nil {
+		panic(err)
+	}
+	configfile.Install()
+	accounting.Start(context.Background())
+	return config.DumpRcBlob()
 }
